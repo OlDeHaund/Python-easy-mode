7343d863f7f003aebf2e8b675d899809ea9db177
Viktor <ravil10096@gmail.com>
2024-11-21 10:01:46 +0200

    Тестовый1

--- Python_easy_mode.py
+++ Python_easy_mode.py
@@ -1,1 +1,37 @@
-print("Hello world")
+from math import pi
+from random import randint as rd
+from math import * #pi laabimoot
+ #ümbernimetus
+# print("Hello world") #Конечное действие для задачи 
+# #intput("Hello world") #Сохраняет введеные данные 
+# nimi = input("Sisestage nimi ").capitalize #lover()-aaa, upper()-AAA, capitalize()-Aaa
+# print("Tere tulemast",nimi)
+# print("tere tulemast "+nimi)
+# vanus=int(input("Kui vana sa oled? "))
+# print("Tere tulemast! Tervitan sind " +nimi+, "sa oled",vanus,"aastat vana")
+# print(f"Tere tulemast! Tervitan sind {nimi} Sa olde {vanus} aastat vana")
+
+#Ülesanne 2
+# vanus = 18
+# eesnimi = "Jaak"
+# pikkus = 16.5
+# kas_käib_koolis = True
+# print(type(vanus))
+# print(type(pikkus))
+# print(type(kas_käib_koolis))
+# if kas_käib_koolis is True, print("Käib kooli")
+
+#Ülesanne 3
+# kokku = rd(1, 1000)
+# print (f"Kokku on {kokku} kommi")
+# kommi = int(input("Mitu kommi sa tahad? "))
+# kokku=kokku-kommi
+# print(f"Teil on jäänud {kokku} kommid")
+
+#Ülesanne4
+print("Läbimõõdu leidmide ")
+#l - ümbermõõt
+l = float(input("Ümbermõõt "))
+d = l/pi
+print(f"Teie läbimõõdu suurus {round(d,2)} sm")
+#If u are reading what think, that means that I still puck upt
