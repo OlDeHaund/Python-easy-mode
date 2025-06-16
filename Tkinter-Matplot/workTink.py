35a22c0dada6c120314ee27ec11b1b52bf6a7112
OlDeHaund <ravil10096@gmail.com>
2025-06-16 08:59:13 +0300

    Add files via upload

---
+++ Tkinter-Matplot/workTink.py
@@ -0,0 +1,53 @@
+from tkinter import *
+from Tund_8_Matplotlib_def import *
+
+global värv
+värv="white"  # Начальный цвет
+
+def värvi_valik():
+    global värv   # Указываем, что работаем с глобальной переменной
+    if tekst.get()!="":
+        tekst.configure(bg="cyan")
+        värv=tekst.get()   # .get - получаем информацию
+    else:
+        tekst.configure(bg="lightblue")
+    
+    figuur(värv)    # Перерисовываем фигуру с новым цветом
+
+def figuur(värv:str):
+    valik=var.get()
+    # värv=värvi_valik()
+    if valik==1:
+        Vaal(värv)
+    elif valik==2:
+        Liblikas(värv)
+    else:
+        Prillid()
+        print("Joonestan hiljem")
+
+
+
+aken=Tk()
+aken.geometry("800x400")  # Задаем размер
+aken.title("Graafikud")
+pealkiri=Label(aken,text="Erinevad piltid Matplotlib abil",font="Poppins 24",fg="purple",bg="cyan", pady=20, width=200)
+
+var=IntVar()
+r1=Radiobutton(aken,text="Vaal",font="Poppins 18",variable=var,value=1,command=lambda:figuur(värv))
+r2=Radiobutton(aken,text="Liblikas",font="Poppins 18",variable=var,value=2,command=lambda:figuur(värv))
+r3=Radiobutton(aken,text="Prillid",font="Poppins 18",variable=var,value=3,command=lambda:figuur(värv))
+tekst=Entry(aken,font="Poppins 24",fg="purple",bg="cyan",width=100)
+nupp=Button(aken,text="Värvi valik",font="Poppins 18",fg="cyan",bg="purple",command=värvi_valik)
+
+
+# Расположение    # place(x=...,y=...), grid(column=...,row=...)
+pealkiri.pack() 
+tekst.pack()
+nupp.pack( )
+r1.pack()
+r2.pack()
+r3.pack()
+
+
+aken.mainloop()
+
